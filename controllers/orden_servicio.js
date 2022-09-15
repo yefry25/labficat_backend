ec20397e750677358a632c5f3b07ba11cc8c8ce5
jeffersonbecerra335@gmail.com <jeffersonbecerra335@gmail.com>
2022-09-14 21:21:49 -0500

    populate

--- controllers/orden_servicio.js
+++ controllers/orden_servicio.js
@@ -1,92 +1,58 @@
-import Orden from "../models/orden_servicio.js"
+import Orden from "../models/orden_servicio.js";
 
 const Ordenes = {
-  ordenPost: async (req, res) => {
-    const { idMuestra, ensayo, realizadoPor, supervisadoPor, observaciones } = req.body
-    try {
-      const orden = new Orden({ idMuestra, ensayo, realizadoPor, supervisadoPor, observaciones })
-
-      if (!orden) {
-        res.status(400).json({ msg: "No se puedo registrar la orden" })
-      }
-      orden.save()
-      res.json({ orden })
-
-    } catch (error) {
-      res.status(500).json({ msg: "Hable con el WebMaster" })
-    }
-  },
-
   ordenGet: async (req, res) => {
     try {
+      const orden = await Orden.find();
+      let ensa = "";
+      /* orden.forEach((item) => {
+        item.itemsorden.forEach((tem)=>{
+          ensa= tem.idensayo
+        })
+      })
+      console.log(ensa); */
+      let palo = await Orden.find().populate({
+        path: "itemsorden.idensayo",
+        populate: { path: "responsables.titular" }
+      });
+      console.log(palo);
 
-      const orden = await Orden.Find()
-
-      if (!orden) {
-        res.json(400).json({ msg: "No se encontro" })
-      }
-
-      res.json({ orden })
-
+      // if (!orden) {
+      //   res.json(400).json({ msg: "No se encontro" })
+      // }
+      res.json({ palo });
     } catch (error) {
-      res.status(500).json({ msg: "Hable con el WebMaster" })
+      res.status(500).json({ msg: "Hable con el WebMaster" });
     }
   },
-  ordenGetRealizado: async (req, res) => {
-    const { realizadoPor } = req.body
-    try {
-      const realizado = await Orden.findById({ realizadoPor })
-      if (!realizado) {
-        res.status(400).json({ msg: "No se encontro" })
-      }
-      res.json({ realizado })
-    } catch (error) {
-      res.status(500).json({ msg: "Hable con el WebMaster" })
-    }
-  },
-
-  ordenGetSupervisado: async (req, res) => {
-    const { supervisadoPor } = req.body
-    try {
-      const supervisado = await Orden.findById({ supervisadoPor })
-      if (!supervisado) {
-        res.status(400).json({ msg: "No se encontro" })
-      }
-      res.json({ supervisado })
-    } catch (error) {
-      res.status(500).json({ msg: "Hable con el WebMaster" })
-    }
-  },
-
   ordenActivar: async (req, res) => {
     const { id } = req.params;
     try {
-      const activar = await Orden.findByIdAndUpdate(id, { estado: 1 })
+      const activar = await Orden.findByIdAndUpdate(id, { estado: 1 });
       if (!activar) {
-        res.status(400).json({ msg: "No se actualizo el estado" })
+        res.status(400).json({ msg: "No se actualizo el estado" });
       }
       res.json({
-        activar
-      })
+        activar,
+      });
     } catch (error) {
-      res.stataus(500).json({ msg: "Hable con el WebMaster" })
+      res.stataus(500).json({ msg: "Hable con el WebMaster" });
     }
   },
-
   ordenDesactivar: async (req, res) => {
     const { id } = req.params;
     try {
-      const desactivar = await Orden.findByIdAndUpdate(id, { estado: 0 })
+      const desactivar = await Orden.findByIdAndUpdate(id, { estado: 0 });
       if (!desactivar) {
-        res.status(400).json({ msg: "No se actualizo el estado" })
+        res.status(400).json({ msg: "No se actualizo el estado" });
       }
       res.json({
-        desactivar
-      })
+        desactivar,
+      });
     } catch (error) {
-      res.stataus(500).json({ msg: "Hable con el WebMaster" })
+      res.stataus(500).json({ msg: "Hable con el WebMaster" });
     }
   },
-}
+};
 
-export default Ordenes
+export default Ordenes;
--- routes/orden_servicio.js
+++ routes/orden_servicio.js
@@ -1,40 +1,19 @@
 import {Router} from "express"
 import { check } from "express-validator"
 import { validarCampos } from "../middlewares/middleware.js"
-import helpersMuestra from "../helpers/muestra.js"
 import helpersUsuario from "../helpers/usuario.js"
-import helpersEnsayo from '../helpers/ensayo.js'
 import Ordenes from "../controllers/orden_servicio.js"
 
 const router = new Router()
 
-router.post('/',[
-    check('idMuestra').custom(helpersMuestra.existeMuestra),
-    check('ensayo').custom(helpersEnsayo.existeEnsayoById),
-    check('realizadoPor',).custom(helpersUsuario.existeUsuarioById),
-    check('supervisadoPor',).custom(helpersUsuario.existeUsuarioById),
-    check('observaciones','no puede estar vacio').not().isEmpty(),
-    validarCampos
-],Ordenes.ordenPost)
-
 router.get('/',Ordenes.ordenGet)
-
-router.get('/realizado',[
-    check('realizadoPor',).custom(helpersUsuario.existeUsuarioById),
-    validarCampos
-],Ordenes.ordenGetRealizado)
-
-router.get('/supervisado',[
-    check('supervisadoPor',).custom(helpersUsuario.existeUsuarioById),
-    validarCampos
-],Ordenes.ordenGetSupervisado)
 
 router.put('/activar/:id',[
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom(helpersUsuario.existeUsuarioById),
 ],Ordenes.ordenActivar)
 
-router.put('/desactivar/:id',[
+router.put('/desactivar/:id',[ 
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom(helpersUsuario.existeUsuarioById),
 ],Ordenes.ordenDesactivar)
